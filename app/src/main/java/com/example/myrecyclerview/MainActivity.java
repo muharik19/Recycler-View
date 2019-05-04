788f33c87310f020eae40e388e49251501fbd6e2
muharik19 <ahmadmuharik@gmail.com>
2019-05-04 23:06:45 +0700

    Recyler View Android Studio 3.4

---
+++ app/src/main/java/com/example/myrecyclerview/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.myrecyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvCategory;
+    private ArrayList<President> list = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvCategory = findViewById(R.id.rv_category);
+        rvCategory.setHasFixedSize(true);
+
+        list.addAll(PresidentData.getListData());
+        showRecyclerList();
+    }
+
+    private void showRecyclerList() {
+        rvCategory.setLayoutManager(new LinearLayoutManager(this));
+        ListPresidentAdapter listPresidentAdapter = new ListPresidentAdapter(this);
+        listPresidentAdapter.setListPresident(list);
+        rvCategory.setAdapter(listPresidentAdapter);
+    }
+
+    private void showRecyclerGrid() {
+        rvCategory.setLayoutManager(new GridLayoutManager(this, 2));
+        GridPresidentAdapter gridPresidentAdapter = new GridPresidentAdapter(this);
+        gridPresidentAdapter.setListPresident(list);
+        rvCategory.setAdapter(gridPresidentAdapter);
+    }
+
+    private void showRecyclerCardView() {
+        rvCategory.setLayoutManager(new LinearLayoutManager(this));
+        CardViewPresidentAdapter cardViewPresidentAdapter = new CardViewPresidentAdapter(this);
+        cardViewPresidentAdapter.setListPresident(list);
+        rvCategory.setAdapter(cardViewPresidentAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_list:
+                showRecyclerList();
+                break;
+            case R.id.action_grid:
+                showRecyclerGrid();
+                break;
+            case R.id.action_cardview:
+                showRecyclerCardView();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
